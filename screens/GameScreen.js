575925221503d991089b082d1a1556ea3afe392b
jvnr965 <54728282+jvnr965@users.noreply.github.com>
2021-08-28 17:02:05 -0300

    style of list and margin on gameScreen responsive

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,6 +6,7 @@ import {
   Button,
   Alert,
   ScrollView,
+  Dimensions,
 } from "react-native";
 import Card from "../components/Card";
 import MainButton from "../components/MainButton";
@@ -105,7 +106,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "90%",
   },
@@ -119,7 +120,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   list: {
-    width: "80%",
+    width: Dimensions.get('window').width > 350 ? '60%' : '80%' ,
     flex: 1,
   },
 });
